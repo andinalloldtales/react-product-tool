04b19422feca4caf6006499d306ad27269d28046
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-04 18:06:58 -0500

    Add Home Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homepage"
 import CreatePage from "./pages/createpage";
 import EditPage from "./pages/editpage";
 
+
 const App = () => {
   return (
     <div>
-     
+      <nav className="bg-gray-800">
+          <div className="container mx-auto p-2"></div>
+            <Link to="/"><h2 className="text-white text-2x1 font-bold">Product Tool</h2></Link>
+      </nav>
         <Routes>
           <Route index element={<HomePage/>}></Route>
           <Route path="/create" element={<CreatePage/>}></Route>

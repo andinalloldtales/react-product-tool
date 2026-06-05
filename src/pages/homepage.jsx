ef3f562fa8d863797e7c3cc8487a047396f3614c
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-05 16:08:39 -0500

    Connect Homepage to Products API

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
+import axios from "axios";
+
 const HomePage = () => {
+
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getProducts = async () => {
+        try {
+            setIsLoading(true)
+            const response = await axios.get("https://backend-product-tool.onrender.com/api/products");
+            console.log(response.data);
+            setProducts(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>The home page</div>
     )
